03330d01bb33830dd8308f001873bf2d0af3d801
Paul Schnau <paul@honey.is>
2015-08-08 19:07:01 -0400

    Twitter works

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
+  User.findOne({where: {id:id}}).then(function(user){
     done(err, user);
   });
 });
@@ -201,58 +201,31 @@ passport.use(new GitHubStrategy(secrets.github, function(req, accessToken, refre
 // Sign in with Twitter.
 
 passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tokenSecret, profile, done) {
-  if (req.user) {
-    return null;
-    // User.findOne({ twitter: profile.id }, function(err, existingUser) {
-    //   if (existingUser) {
-    //     req.flash('errors', { msg: 'There is already a Twitter account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-    //     done(err);
-    //   } else {
-    //     User.findById(req.user.id, function(err, user) {
-    //       user.twitter = profile.id;
-    //       user.tokens.push({ kind: 'twitter', accessToken: accessToken, tokenSecret: tokenSecret });
-    //       user.profile.name = user.profile.name || profile.displayName;
-    //       user.profile.location = user.profile.location || profile._json.location;
-    //       user.profile.picture = user.profile.picture || profile._json.profile_image_url_https;
-    //       user.save(function(err) {
-    //         req.flash('info', { msg: 'Twitter account has been linked.' });
-    //         done(err, user);
-    //       });
-    //     });
-    //   }
-    // });
+  User.findOne({where: {twitter: profile.id}}).then(
+    function(user){
+      if (user === null){
+        var new_user = User.build({
+          email: profile.username + "@twitter.com",
+          twitter: profile.id,
+          profile_name: profile.displayName,
+          profile_location: profile._json.location,
+          profile_picture: profile._json.profile_image_url_https,
+        });
 
-  } else {
-    
-    var user = User.build({
-      email: profile.username + "@twitter.com",
-      twitter: profile.id,
-      profile_name: profile.displayName,
-      profile_location: profile._json.location,
-      profile_picture: profile._json.profile_image_url_https,
-    })
-
-     user.save(function(err) {
-        done(err, user);
-      });
-
-    // User.findOne({ twitter: profile.id }, function(err, existingUser) {
-    //   if (existingUser) return done(null, existingUser);
-    //   var user = new User();
-    //   // Twitter will not provide an email address.  Period.
-    //   // But a person’s twitter username is guaranteed to be unique
-    //   // so we can "fake" a twitter email address as follows:
-    //   user.email = profile.username + "@twitter.com";
-    //   user.twitter = profile.id;
-    //   user.tokens.push({ kind: 'twitter', accessToken: accessToken, tokenSecret: tokenSecret });
-    //   user.profile.name = profile.displayName;
-    //   user.profile.location = profile._json.location;
-    //   user.profile.picture = profile._json.profile_image_url_https;
-    //   user.save(function(err) {
-    //     done(err, user);
-    //   });
-    // });
-  }
+        new_user.save(function(err) {
+          done(err, user);
+        });
+      } else {
+        user.email = profile.username + "@twitter.com";
+        user.twitter = profile.id;
+        user.profile_name= profile.displayName;
+        user.profile_location= profile._json.location;
+        user.profile_picture= profile._json.profile_image_url_https;
+        user.save(function(err) {
+          done(err, user);
+        });
+      }
+    });
 }));
 
 /**
