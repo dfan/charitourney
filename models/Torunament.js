61e52ed4b72de0719a5168050af9b0bb721a5107
Casey Chow <casey.chow@gmail.com>
2015-08-08 16:28:50 -0400

    remove Torunament.js

--- models/Torunament.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var tournamentSchema = new mongoose.Schema({
-  name: { type: String, default: '' },
-  charities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Charity' }],
-  battles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Battle'}]
-});
-
-module.exports = mongoose.model('Tournament', tournamentSchema);
