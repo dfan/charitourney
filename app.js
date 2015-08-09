93b08db05d531d255d41a13f60115073e6e6c462
Paul Schnau <paul@honey.is>
2015-08-08 22:19:33 -0400

    /api redirect'

--- app.js
+++ app.js
@@ -121,6 +121,10 @@ app.post('/account/password', passportConf.isAuthenticated, userController.postU
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
 
+app.get('/api', function (req, res) {
+  res.redirect('/tournament');
+});
+
 
 /**
  * OAuth authentication routes. (Sign in)
